7deba3f60bcff847ddd49757cea3c4b677b9137c
moovendhan_meyi <moovendhan.v@meyicloud.com>
2025-04-10 11:53:53 +0530

    changes in deploy.sh

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -2,9 +2,10 @@
 export BUCKET_NOT_EXIST=100
 IsS3BucketExists=true
 
-try (
+# Try to list the objects in the S3 bucket
+try {
     aws s3api list-objects --bucket $PipelineArtifactBucket --no-cli-pager || throw $BUCKET_NOT_EXIST
-) catch || {
+} catch || {
     case $exception_code in
         $BUCKET_NOT_EXIST)
             echo "Bucket does not exist"
@@ -23,20 +24,21 @@ sam deploy -t cloudformation.yaml \
            --region=$Region \
            --no-fail-on-empty-changeset \
            --capabilities=CAPABILITY_NAMED_IAM CAPABILITY_IAM \
-           --parameter-overrides Tags=${Tags} \
-                                 Stage=${Stage} \
-                                 Region=${Region} \
-                                 Prefix=${Prefix} \
-                                 RepoName=${RepoName} \
-                                 GitBranch=${GitBranch} \
-                                 Environment=${Environment} \
-                                 XrayEnabled=${XrayEnabled} \
-                                 GitHubOwner=${GitHubOwner} \
-                                 ResourceStackName=${ResourceStackName} \
-                                 GitHubRepositoryName=${GitHubRepositoryName} \
-                                 CodeStarConnectionArn=${CodeStarConnectionArn} \
-                                 PipelineArtifactBucket=${PipelineArtifactBucket} \
-                                 S3Bucket=${S3Bucket} \
-                                 S3Key=${S3Key} \
-                                 UserPoolId=${UserPoolId} \
-                                 IsS3BucketExists=${IsS3BucketExists}
+           --parameter-overrides \
+               Tags=${Tags} \
+               Stage=${Stage} \
+               Region=${Region} \
+               Prefix=${Prefix} \
+               RepoName=${RepoName} \
+               GitBranch=${GitBranch} \
+               Environment=${Environment} \
+               XrayEnabled=${XrayEnabled} \
+               GitHubOwner=${GitHubOwner} \
+               ResourceStackName=${ResourceStackName} \
+               GitHubRepositoryName=${GitHubRepositoryName} \
+               CodeStarConnectionArn=${CodeStarConnectionArn} \
+               PipelineArtifactBucket=${PipelineArtifactBucket} \
+               S3Bucket=${S3Bucket} \
+               S3Key=${S3Key} \
+               UserPoolId=${UserPoolId} \
+               IsS3BucketExists=${IsS3BucketExists}
